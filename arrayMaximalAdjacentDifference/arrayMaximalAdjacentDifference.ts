a9330199d3b3c72caa17d95ee48fbffd9fed0d8a
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2020-02-04 08:03:06 +0100

    arrayMaximalAdjacentDifference

---
+++ arrayMaximalAdjacentDifference/arrayMaximalAdjacentDifference.ts
@@ -0,0 +1,14 @@
+function arrayMaximalAdjacentDifference(inputArray: number[]): number {
+  let result: number[] = [];
+  let max: number = 0;
+
+  for (let i = 0; i < inputArray.length - 1; i++) {
+    if (i + 1) {
+      result.push(Math.abs(inputArray[i] - inputArray[i + 1]));
+    }
+  }
+  
+  return Math.max.apply(null, result)
+}
+
+console.log(arrayMaximalAdjacentDifference([2, -7, 1, 0]));
