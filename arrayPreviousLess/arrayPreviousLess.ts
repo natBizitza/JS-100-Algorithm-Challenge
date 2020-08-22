e91ca90adda037ec1bf7aba06df048b8897be89a
Natalia Romankevich <40891986+natBizitza@users.noreply.github.com>
2020-08-22 23:24:24 +0200

    Solved

--- arrayPreviousLess/arrayPreviousLess.ts
+++ arrayPreviousLess/arrayPreviousLess.ts
@@ -1,5 +1,14 @@
 function arrayPreviousLess(items: number[]): number[] {
-
+    let res = [-1];
+    for(let i=1; i<items.length;i++){
+        if(items[i-1]<items[i]){
+            res.push(items[i-1]);
+        }else{
+            res.push(-1)
+        }
+    }
+    return res;
 }
+
 
 console.log(arrayPreviousLess([3, 5, 2, 4, 5]));
